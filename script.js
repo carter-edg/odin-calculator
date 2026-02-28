7224bba3b50cb7ac5b9f753473bbe25f3cbf599a
Carter Edginton <249395916+carter-edg@users.noreply.github.com>
2026-02-28 14:13:37 -0700

    Button pressing fires events
    Currently the inside of the event handler just logs to the console.

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ function operate(left, operator, right){
     if (operator === "divide"){return divide(left,right);}
     else{return NaN;}
 }
+
+let numpadButtons = [...document.querySelectorAll("#numpad button")];
+let operatorButtons = [...document.querySelectorAll("#operators button")];
+numpadButtons.forEach((numpadButton)=>{
+    numpadButton.addEventListener("click", (event)=>{
+        numpadButtonClicked(numpadButton.textContent);
+    });
+});
+
+operatorButtons.forEach((operatorButton)=>{
+    operatorButton.addEventListener("click", (event)=>{
+        operatorButtonClicked(operatorButton.textContent);
+    });
+});
+
+function numpadButtonClicked(value){
+    console.log(value);
+}
+
+function operatorButtonClicked(value){
+    console.log(value);
+}
