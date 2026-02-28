f470c84f7336ceae5954d496dd59473f5b75d045
Carter Edginton <249395916+carter-edg@users.noreply.github.com>
2026-02-28 15:41:24 -0700

    error checking, rounding result, added comments

--- script.js
+++ script.js
@@ -2,32 +2,39 @@ let leftNum  = "";
 let rightNum = "";
 let operator = "";
 
+//add two numbers
 function add(a, b){
     return a + b;
 }
 
+//subtract two numbers
 function subtract(a,b){
     return a-b;
 }
 
+//multiply two numbers
 function multiply(a,b){
     return a*b;
 }
 
+//divide two numbers
 function divide(a,b){
     return a/b;
 }
 
+//figure out which operation to perform given the operator, left number, and right number.
 function operate(left, operator, right){
     if (operator === "+"){return add(left,right);}
     if (operator === "-"){return subtract(left,right);}
     if (operator === "*"){return multiply(left,right);}
     if (operator === "/"){return divide(left,right);}
-    else{return "OOPS!!!";}
+    else{return "OOPS!!!";}//should never happen.
 }
 
+//grabs screen item for display updates.
 const screen = document.querySelector("#screen");
 
+//gets all buttons and adds appropriate events to them.
 let numpadButtons = [...document.querySelectorAll("#numpad button")];
 let operatorButtons = [...document.querySelectorAll("#operators button")];
 numpadButtons.forEach((numpadButton)=>{
@@ -35,20 +42,23 @@ numpadButtons.forEach((numpadButton)=>{
         numpadButtonClicked(numpadButton.textContent);
     });
 });
-
 operatorButtons.forEach((operatorButton)=>{
     operatorButton.addEventListener("click", (event)=>{
         operatorButtonClicked(operatorButton.textContent);
     });
 });
 
+//updates the screen.
 function updateDisplay(){
-    screen.textContent = leftNum + operator + rightNum;
+    const leftNumDisplay = +(+leftNum).toFixed(5);//displays as a 5 digit(max) float
+    screen.textContent = leftNumDisplay + operator + rightNum;
 }
 
+//event when a button in the numpad is clicked. 
 function numpadButtonClicked(value){
     console.log(value);
-    if (operator === ""){
+    if (operator === ""){//determine whether editing left number or right number
+        if (Number.isNaN(Number(leftNum))){leftNum = "";updateDisplay();}//clears error if there was
         leftNum+=value;//held as string
     }else{
         rightNum+=value;//held as string
@@ -56,6 +66,7 @@ function numpadButtonClicked(value){
     updateDisplay();
 }
 
+//event when a button in the operators section is clicked.
 function operatorButtonClicked(value){
     console.log(value);
     if (value === "="){
@@ -63,7 +74,7 @@ function operatorButtonClicked(value){
     }else if (value === "c"){
         leftNum = operator = rightNum = "";
         updateDisplay();
-    }else{
+    }else{ //+,-,*,/ operators
         operator = value;
         updateDisplay();
     }
@@ -71,10 +82,13 @@ function operatorButtonClicked(value){
 
 //performs the = operator, clears display.
 function calculate(){
+    if (rightNum === ""){leftNum = "OOPS!!!"; operator = ""; updateDisplay(); return;}//error check
+    //leftNum and rightNum are stored as strings.
     let a = Number(leftNum);
     let b = Number(rightNum);
     let result = operate(a, operator, b);
-    leftNum = String(result);
+    leftNum = String(+(result.toFixed(5)));
+    //clear non-result, update display
     operator = rightNum = "";
     updateDisplay();
 }
